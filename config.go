abd03800387a8e9e8c300c69adec0d5ebe44f447
leodotcloud <leodotcloud@gmail.com>
2016-11-09 22:59:27 -0700

    Restructing the code in separate files

---
+++ config.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/containernetworking/cni/pkg/types"
+)
+
+// NetConf is used to hold the config of the network
+type NetConf struct {
+	types.NetConf
+	BrName          string `json:"bridge"`
+	BrSubnet        string `json:"bridgeSubnet"`
+	BrIP            string `json:"bridgeIP"`
+	IsGW            bool   `json:"isGateway"`
+	IsDefaultGW     bool   `json:"isDefaultGateway"`
+	IPMasq          bool   `json:"ipMasq"`
+	MTU             int    `json:"mtu"`
+	LinkMTUOverhead int    `json:"linkMTUOverhead"`
+	HairpinMode     bool   `json:"hairpinMode"`
+}
+
+func loadNetConf(bytes []byte) (*NetConf, error) {
+	n := &NetConf{
+		BrName: defaultBrName,
+	}
+	if err := json.Unmarshal(bytes, n); err != nil {
+		return nil, fmt.Errorf("failed to load netconf: %v", err)
+	}
+	return n, nil
+}
